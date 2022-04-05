874cdf2941d7d3ab95f2fb87a6594d4316101797
Walter <dalewaltergh@gmail.com>
2022-04-05 19:03:38 +0800

    stocks turbo_frame

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,5 +1,9 @@
 class Stock < ApplicationRecord
   has_many :invesments, dependent: :destroy
+  validates_presence_of :symbol, :company_name, 
+    :latest_price, :quantity
 
-  validates :company_name, presence: true
+  after_create_commit { broadcast_prepend_to "stocks" }
+  after_update_commit { broadcast_replace_to "stocks" }
+  after_destroy_commit { broadcast_remove_to "stocks" }
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,10 +1,5 @@
 # frozen_string_literal: true
 
-# Assuming you have not yet modified this file, each configuration option below
-# is set to its default value. Note that some are commented out while others
-# are not: uncommented lines are intended to protect your configuration from
-# breaking changes in upgrades (i.e., in the event that future versions of
-# Devise change the default values for those options).
 class TurboFailureApp < Devise::FailureApp
   def respond
     if request_format == :turbo_stream
@@ -15,10 +10,36 @@ class TurboFailureApp < Devise::FailureApp
   end
 
   def skip_format?
-    %w[html turbo_stream].include?(request_format.to_s)
+    %w[html turbo_stream].include? request_format.to_s
   end
 end
+
+class TurboDeviseController < ApplicationController
+  class Responder < ActionController::Responder
+    def to_turbo_stream
+      controller.render(options.merge(formats: :html))
+    rescue ActionView::MissingTemplate => err
+      if get?
+        raise err
+      elsif has_errors? && default_action
+        render rendering_options.merge(formats: :html, status: :unprocessable_entity)
+      else
+        redirect_to navigation_location
+      end
+    end
+  end
+
+  self.responder = Responder
+  respond_to :html, :turbo_stream
+end
+
+# Assuming you have not yet modified this file, each configuration option below
+# is set to its default value. Note that some are commented out while others
+# are not: uncommented lines are intended to protect your configuration from
+# breaking changes in upgrades (i.e., in the event that future versions of
+# Devise change the default values for those options).
 # Use this hook to configure devise mailer, warden hooks and so forth.
+
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
   # The secret key used by Devise. Devise uses this key to generate
@@ -30,7 +51,7 @@ Devise.setup do |config|
 
   config.parent_controller = "TurboDeviseController"
   config.navigational_formats = ['*/*', :html, :turbo_stream]
-  config.warden do |manager| 
+  config.warden do |manager|
     manager.failure_app = TurboFailureApp
   end
 
