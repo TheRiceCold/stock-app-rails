e2b6f5d3354a881ac80083ecfbf88782d4759ca9
quevon <quevonmontojo@gmail.com>
2022-04-17 08:53:44 +0800

    added sending email when deactivating user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,19 +3,17 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     if @user.update approved: true
       UserApprovalEmailMailer.with(user: @user).userapproved.deliver_later
-
-      redirect_to admin_users_path
       flash[:success] = "You have been activated #{@user.email}"
-      #send email here
+      redirect_to admin_users_path
     end
   end
 
   def inactive
     @user = User.find(params[:id])
     if @user.update approved: false
-      redirect_to admin_users_path
+      InactivateUserMailer.with(user: @user).inactiveuser.deliver_later
       flash[:success] = "You have been deactivated #{@user.email}"
-
+      redirect_to admin_users_path
     end
   end
 end
