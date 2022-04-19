5d5406c0d4fedcde582c632d7f1b8c7c0e56c3ac
quevon <quevonmontojo@gmail.com>
2022-04-19 11:38:46 +0800

    test validations in modal user and admin

--- spec/models/admin_user_spec.rb
+++ spec/models/admin_user_spec.rb
@@ -1,7 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe AdminUser, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # pending "add some examples to (or delete) #{__FILE__}"
 
-  it "is not valid without a email"
+  it "is not valid without a email" do
+    admin = AdminUser.new(email: nil)
+    expect(admin).to_not be_valid
+  end
+  it "is not valid without a password" do
+    admin = AdminUser.new(password: nil)
+    expect(admin).to_not be_valid
+  end
 end
+
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # pending "add some examples to (or delete) #{__FILE__}
+  it "is not valid without a email" do
+    user = User.new(email: nil)
+    expect(user).to_not be_valid
+  end
+  it "is not valid without a password" do
+    user = User.new(password: nil)
+    expect(user).to_not be_valid
+  end
 end
