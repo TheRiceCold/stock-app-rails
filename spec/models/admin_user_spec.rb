1cd1d33c117b2322059e52d6a86024bc9d3ae1ae
quevon <quevonmontojo@gmail.com>
2022-04-19 12:11:32 +0800

    added rspec testing in admin that email should be unique

--- spec/models/admin_user_spec.rb
+++ spec/models/admin_user_spec.rb
@@ -11,5 +11,18 @@ RSpec.describe AdminUser, type: :model do
     admin = AdminUser.new(password: nil)
     expect(admin).to_not be_valid
   end
+
+  it "is not unique email" do
+    first_email = AdminUser.create(email: "quevon@gmail.com", password: '123456')
+    second_email =  AdminUser.create(email: "quevon@gmail.com", password: '123456')
+  
+    expect(second_email).to_not be_valid
+    expect(second_email.errors).to be_present
+    expect(second_email.errors.to_hash.keys).to include(:email)
+    expect(second_email.errors[:email]).to include("has already been taken")
+  
+  end
+
+
 end
 
