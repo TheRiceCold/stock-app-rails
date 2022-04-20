313a6d7e6935d7939aa6a18b1f1ec0c78bbdf84e
quevon <quevonmontojo@gmail.com>
2022-04-20 13:46:41 +0800

    test

--- app/javascript/controllers/auth_controller.js
+++ app/javascript/controllers/auth_controller.js
@@ -1,41 +1,41 @@
-import inputAnim from "../input_anim"
-import { Controller } from "@hotwired/stimulus"
+// import inputAnim from "../input_anim"
+// import { Controller } from "@hotwired/stimulus"
 
-// Connects to data-controller="auth"
-export default class extends Controller {
-  static targets = ["input", "pwdIcon", "btnSubmit"]
+// // Connects to data-controller="auth"
+// export default class extends Controller {
+//   static targets = ["input", "pwdIcon", "btnSubmit"]
   
-  initialize() {
-    const inputs = this.inputTargets
-    const pwdInputs = inputs.filter(i => i.type === "password")
+//   initialize() {
+//     const inputs = this.inputTargets
+//     const pwdInputs = inputs.filter(i => i.type === "password")
 
-    inputAnim(inputs)
-    this.passwordInput(pwdInputs)
-  }
+//     inputAnim(inputs)
+//     this.passwordInput(pwdInputs)
+//   }
 
-  // Private
-  passwordInput(inputs) {
-    const pwdIcons = this.pwdIconTargets
+//   // Private
+//   passwordInput(inputs) {
+//     const pwdIcons = this.pwdIconTargets
 
-    inputs.forEach((input, i) => {
-      const icon = pwdIcons[i]
+//     inputs.forEach((input, i) => {
+//       const icon = pwdIcons[i]
 
-      input.onkeyup = () => {
-        if (input.value.trim())
-          icon.classList.add("show")
-        else 
-          icon.classList.remove("show")
-      }
+//       input.onkeyup = () => {
+//         if (input.value.trim())
+//           icon.classList.add("show")
+//         else 
+//           icon.classList.remove("show")
+//       }
 
-      icon.onclick = () => {
-        if (icon.classList.contains("fa-eye")) {
-          input.setAttribute("type", "text")
-          icon.classList.replace("fa-eye", "fa-eye-slash")
-        } else {
-          input.setAttribute("type", "password")
-          icon.classList.replace("fa-eye-slash", "fa-eye")
-        }
-      }
-    })
-  } 
-}
+//       icon.onclick = () => {
+//         if (icon.classList.contains("fa-eye")) {
+//           input.setAttribute("type", "text")
+//           icon.classList.replace("fa-eye", "fa-eye-slash")
+//         } else {
+//           input.setAttribute("type", "password")
+//           icon.classList.replace("fa-eye-slash", "fa-eye")
+//         }
+//       }
+//     })
+//   } 
+// }
