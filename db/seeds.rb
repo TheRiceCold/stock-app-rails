779ad63b204e04dd8e3f7c7d178fc1eee9014c22
quevon <quevonmontojo@gmail.com>
2022-04-20 11:42:34 +0800

    admin seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@ end if Rails.env.development?
 
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
 
+AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
