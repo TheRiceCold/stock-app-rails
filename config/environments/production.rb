e23e90af78a3db0693824f49b66e175f9f5ba6d4
quevon <quevonmontojo@gmail.com>
2022-04-20 12:47:45 +0800

    test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,7 +21,6 @@ Rails.application.configure do
   config.action_mailer.default_url_options = {
     host: 'https://stocks-app-project.herokuapp.com/users/sign_in'
   }
-  ActionMailer::Base.delivery_method = :smtp
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
