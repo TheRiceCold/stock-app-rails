b79b890e51287cc0f9fb45838c289a22fcd884cd
quevon <quevonmontojo@gmail.com>
2022-04-20 11:59:22 +0800

    test

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,9 +16,9 @@ Rails.application.configure do
 
   #DEVISE SECTION
 
-  config.action_mailer.default_url_options = {
-    host: 'localhost:3000'
-  }
+  # config.action_mailer.default_url_options = {
+  #   host: 'localhost:3000'
+  # }
   ###
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
